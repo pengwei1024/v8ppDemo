ee2f461f748486470fb565bd9af48c0ec6941067
pengwei08 <pengwei08@baidu.com>
2021-05-11 14:28:34 +0800

    生成快照的示例

--- main2.cpp
+++ main2.cpp
@@ -6,20 +6,36 @@
 
 #define MAKE_SNAPSHOT
 
+struct PersistentWrapper {
+    v8::Persistent<v8::Object> persistent;
+    char* msg{};
+};
+
 void nativeMethod(const v8::FunctionCallbackInfo<v8::Value> &args) {
     printf("call nativeMethod\n");
+    v8::HandleScope handle_scope(args.GetIsolate());
     v8::Local<v8::Object> object = args.Holder();
     printf("InternalFieldCount=%d\n", object->InternalFieldCount());
     v8::Local<v8::External> wrap = v8::Local<v8::External>::Cast(object->GetInternalField(0));
-    printf("InternalFieldCount=%d ptr=%p\n", object->InternalFieldCount(),wrap->Value());
+    printf("InternalFieldCount=%d ptr=%p\n", object->InternalFieldCount(), wrap->Value());
     if (!wrap.IsEmpty()) {
-        char * s = (char *)wrap->Value();
-        printf("InternalField = %s\n", s);
+        auto* wrapper = static_cast<PersistentWrapper *>(wrap->Value());
+        printf("InternalField = %s\n", wrapper->msg);
     }
     args.GetReturnValue().Set(
             v8::String::NewFromUtf8(args.GetIsolate(), v8::V8::GetVersion())
                     .ToLocalChecked());
 }
+
+
+
+void WeakCallback(const v8::WeakCallbackInfo<PersistentWrapper> & data) {
+    printf("call WeakCallback");
+    PersistentWrapper* wrapper = data.GetParameter();
+    wrapper->persistent.Reset();
+    delete wrapper;
+}
+
 
 int main(int argc, char *argv[]) {
     // Initialize V8.
@@ -29,25 +45,21 @@ int main(int argc, char *argv[]) {
     v8::V8::InitializePlatform(platform.get());
     v8::V8::Initialize();
     printf("version=%s\n", v8::V8::GetVersion());
+#ifndef MAKE_SNAPSHOT
     v8::StartupData initData = {nullptr, 0};
     SnapshotUtil::readFile(initData);
+#endif
     std::vector<intptr_t> external_references;
-    external_references.push_back((intptr_t)nativeMethod);
-    char* abc = "hello";
-    external_references.push_back((intptr_t)abc);
+    external_references.push_back((intptr_t) nativeMethod);
+    auto* persistentWrapper = new PersistentWrapper();
+    persistentWrapper->msg = "abc";
+    external_references.push_back((intptr_t) persistentWrapper);
 #ifndef MAKE_SNAPSHOT
     v8::SnapshotCreator create(external_references.data(), &initData);
 #else
-    v8::SnapshotCreator create(external_references.data());
+    auto *create = new v8::SnapshotCreator(external_references.data());
 #endif
-//    auto isolate_ = v8::Isolate::Allocate();
-//    v8::Isolate::Initialize(isolate_, create_params);
-//    isolate_->Enter();
-//    v8::Isolate* isolate_ = v8::Isolate::New(create_params);
-    auto isolate_ = create.GetIsolate();
-//    v8::Isolate::Initialize(isolate_, create_params);
-//    creator = &create;
-
+    auto isolate_ = create->GetIsolate();
     {
         v8::Isolate::Scope isolate_scope(isolate_);
         v8::HandleScope handle_scope(isolate_);
@@ -60,15 +72,18 @@ int main(int argc, char *argv[]) {
         global->Set(v8::String::NewFromUtf8(isolate_, "_na").ToLocalChecked(), swanObjTpl);
         v8::Local<v8::FunctionTemplate> fun = v8::FunctionTemplate::New(isolate_, &nativeMethod);
         swanObjTpl->Set(isolate_, "nativeMethod", fun);
-
         v8::Local<v8::Object> wrapper = swanObjTpl->NewInstance(context).ToLocalChecked();
         if (swanObjTpl->NewInstance(context).ToLocal(&wrapper)) {
-            wrapper->SetInternalField(0, v8::External::New(isolate_, abc));
+            wrapper->SetInternalField(0, v8::External::New(isolate_, persistentWrapper));
         }
-        context->Global()->Set(context, v8::String::NewFromUtf8(isolate_, "_na").ToLocalChecked(), wrapper)
-                .IsJust();
-        create.AddContext(context);
-        create.SetDefaultContext(context);
+
+        persistentWrapper->persistent.Reset(isolate_, wrapper);
+        persistentWrapper->persistent.SetWeak(persistentWrapper, WeakCallback, v8::WeakCallbackType::kInternalFields);
+        create->AddData(wrapper);
+        context->Global()->Set(context, v8::String::NewFromUtf8(isolate_, "_na")
+                .ToLocalChecked(), wrapper).IsJust();
+        create->AddContext(context);
+        create->SetDefaultContext(context);
 #endif
 
         // Enter the context for compiling and running the hello world script.
@@ -76,6 +91,7 @@ int main(int argc, char *argv[]) {
         {
             v8::Local<v8::String> source =
                     v8::String::NewFromUtf8(isolate_, "_na.nativeMethod() + ' - hello' + 123").ToLocalChecked();
+//                    v8::String::NewFromUtf8(isolate_, "' - hello' + 123").ToLocalChecked();
 
             // Compile the source code.
             v8::Local<v8::Script> script =
@@ -90,12 +106,13 @@ int main(int argc, char *argv[]) {
         }
     }
 #ifdef MAKE_SNAPSHOT
-    auto data = create.CreateBlob(v8::SnapshotCreator::FunctionCodeHandling::kClear);
+    auto data = create->CreateBlob(v8::SnapshotCreator::FunctionCodeHandling::kClear);
     printf("CreateBlob data=%d\n", data.raw_size);
     SnapshotUtil::writeFile(data);
 #endif
-
-    isolate_->Dispose();
+//    isolate_->Exit();
+//    isolate_->Dispose();
+    delete create;
     v8::V8::Dispose();
     v8::V8::ShutdownPlatform();
 }
